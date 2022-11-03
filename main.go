62f28ce0a94895878e1fba9c1703303b1b0c713d
Aeonn <theodebauvais@protonmail.com>
2022-11-03 19:34:26 +0100

    Added yaml config-parser with an example file.

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	config "nid-go/cmd"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	var configPath string
+
+	if len(os.Args) == 2 {
+		configPath = os.Args[1]
+	} else {
+		configPath = "nid-go.yaml"
+	}
+
+	res, err := config.ParseConfigFile(configPath)
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("%v", res)
 }
